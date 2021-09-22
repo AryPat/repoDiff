ad13739942e81151594f41227c9ad578d689ea6c
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-09-22 11:20:15 -0400

    Added info template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,8 @@ const Title = styled.div`
 const Buttons = styled.div`
   display: flex;
   flex-direction: column; 
+  justify-content: space-evenly;
+  align-items: space-evenly;
   width: 30%;
 `;
 
@@ -64,19 +66,66 @@ const Enter = styled.button`
   }
 `;
 
+const CardContainer = styled.div`
+  width: 50%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+
+  background: #ffffff;
+  border: 5px solid #f5f7fa;
+  box-sizing: border-box;
+  box-shadow: 0px 8px 24px #eff3f9;
+  border-radius: 20px;
+  padding: 1rem;
+  margin-top: 3rem;
+`;
+
+const CardInfo = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  
+`;
+
+const Info = styled.div`
+  width:50%;
+  height: 90%;
+  
+  background: #ffffff;
+  border: 5px solid #f5f7fa;
+  box-sizing: border-box;
+  box-shadow: 0px 8px 24px #eff3f9;
+  border-radius: 40px;
+  padding: 1rem;
+
+`;
+
+const Number = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 export default function App() {
   const [first, setFirst] = useState("");
+  const [result, setResult] = useState({});
+  const [percentage, setPercentage] = useState(0);
+  const [gitHubLink, setGitHubLink] = useState("");
 
   useEffect(() => {
     first.length
       ? (document.getElementById("buttonControl").disabled = false)
       : (document.getElementById("buttonControl").disabled = true);
-  });
+  }, [result, percentage]);
 
   return (
     <Container>
       <Typist cursor={{show: false}}  ms={5000} avgTypingSpeed={30000}>
-        <Title>Repo Comparer</Title>
+        <Title>oBITuary</Title>
       </Typist>
       
       <Buttons>
@@ -92,6 +141,15 @@ export default function App() {
           Enter
         </Enter>
       </Buttons>
+
+      <CardContainer>
+        <h3 style={{fontSize: '1.5rem', paddingLeft: '1rem'}}>{first}</h3>
+        <CardInfo>
+          <Info>this is the information</Info>
+          <number>{percentage}%</number>
+        </CardInfo>
+      </CardContainer>
+      
     </Container>
   );
 }
